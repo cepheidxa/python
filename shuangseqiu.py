3309d580f00f4e3e46e43b7e659a535010ec7372
zhangbo <zbsdta@126.com>
2018-01-28 12:35:49 +0800

    change shuangseqiu.py, add order

--- shuangseqiu.py
+++ shuangseqiu.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
 
-import random2
+import time
+import random
 
 class ShuangSeQiu:
-    def __init__(self):
-        self.__r = random2.Random()
+    def __init__(self, order = None):
+        self.__r = random.SystemRandom()
+        if not order:
+            self.__order = 1
+        else:
+            self.__order = order
+    def randint(self, min, max):
+        start = time.time()
+        global curr
+        global r
+        curr = start
+        length = max - min + 1
+        r = self.__r.getrandbits(8) % length
+        while curr < start + self.__order:
+            curr = time.time()
+            r += self.__r.getrandbits(8)
+            r %= length
+        return r
     def randRed(self):
-        return self.__r.randint(1, 33)
+        return self.randint(1, 32)
     def randBlue(self):
-        return self.__r.randint(1, 16)
+        return self.randint(1, 16)
     def cast(self):
         red = set()
         while len(red) < 6:
@@ -17,6 +34,6 @@ class ShuangSeQiu:
         return (sorted(red), blue)
             
 if __name__ == "__main__":
-    a = ShuangSeQiu()
+    a = ShuangSeQiu(300)
     for i in range(2):
         print(a.cast())
