cbaf5282a87e616a899fe637d1a70069ffd8642f
zhangbo <zbsdta@126.com>
2018-01-28 12:37:24 +0800

    add more control options

--- passgen.py
+++ passgen.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 
 from argparse import ArgumentParser
-from random import SystemRandom
-from string import ascii_letters, digits, punctuation
+from secrets import SystemRandom
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 if __name__ == "__main__":
     parser = ArgumentParser(description = "Generate random password.")
+    parser.add_argument("-n",action = 'store_true', help = "Have numberss in the passwrod")
+    parser.add_argument("-a",action = 'store_true', help = "Have lowercase characters in the passwrod")
+    parser.add_argument("-A",action = 'store_true', help = "Have uppercase characters in the passwrod")
     parser.add_argument("-s",action = 'store_true', help = "Have symbols in the passwrod")
     parser.add_argument("-l", type = int, metavar= "length", help = "The length of password", default = 16)
     command = parser.parse_args()
 
     r = SystemRandom()
     password = [] 
-    valid_chars = ascii_letters + digits
+    valid_chars = ''
+    if command.n:
+        valid_chars += digits
+    if command.a:
+        valid_chars += ascii_lowercase
+    if command.A:
+        valid_chars += ascii_uppercase
     if command.s:
-        valid_chars = valid_chars + punctuation
+        valid_chars += punctuation
+    if not valid_chars:
+        valid_chars = digits
     print("".join([r.choice(valid_chars) for i in range(command.l)]))
